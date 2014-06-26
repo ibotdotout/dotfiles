9268d92194ba75b456d77d46fcfe142b229c1fd8
ibotdotout <tkroputa@gmail.com>
2014-06-27 01:53:55 +0700

    remove auto cd to  in tmux

--- tmux/tmux_link_setting.sh
+++ tmux/tmux_link_setting.sh
@@ -5,9 +5,3 @@ if [ -f "$HOME/.tmux.conf" ]; then
   mv ~/$FILE ~/$FILE.old
 fi
 ln -s $PWD/$FILE ~/$FILE
-
-FILE=".tmux-panes"
-if [ -f "$HOME/.tmux.panes" ]; then
-  mv ~/$FILE ~/$FILE.old
-fi
-ln -s $PWD/$FILE ~/$FILE
