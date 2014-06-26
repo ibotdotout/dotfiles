c4055ceea1e8e9c5c2adda2adbf68838b3fd06dc
ibotdotout <tkroputa@gmail.com>
2014-06-27 01:06:11 +0700

    let tmux cd current dir when open new pane

--- tmux/tmux_link_setting.sh
+++ tmux/tmux_link_setting.sh
@@ -5,3 +5,9 @@ if [ -f "$HOME/.tmux.conf" ]; then
   mv ~/$FILE ~/$FILE.old
 fi
 ln -s $PWD/$FILE ~/$FILE
+
+FILE=".tmux-panes"
+if [ -f "$HOME/.tmux.panes" ]; then
+  mv ~/$FILE ~/$FILE.old
+fi
+ln -s $PWD/$FILE ~/$FILE
