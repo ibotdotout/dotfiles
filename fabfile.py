5f2cb7179ca4d7c35f221c668f54756b35e7050d
botbotbot <tkroputa@gmail.com>
2014-09-08 21:43:05 +0700

    Update fabfile.py
    refactor code

--- fabfile.py
+++ fabfile.py
@@ -42,13 +42,12 @@ def intall_git():
     
 
 def install_janus_vim():
-    sudo("sudo apt-get install ruby-dev rake exuberant-ctags ack-grep")
+    sudo("apt-get install ruby-dev rake exuberant-ctags ack-grep -y")
     run("wget https://bit.ly/janus-bootstrap -O - | bash")
     
 
 def install_docker_io():
-    sudo("sudo apt-get update")
-    sudo("sudo apt-get install docker.io")
+    sudo("apt-get update & apt-get install docker.io -y")
 
 
 def set_devenv():
