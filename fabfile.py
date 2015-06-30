766c0a4c3c8b7a6046e2b8edb101443a9025d0c8
ibotdotout <tkroputa@gmail.com>
2015-06-30 18:57:54 +0700

    update fabric for koding.io

--- fabfile.py
+++ fabfile.py
@@ -7,61 +7,76 @@
 # usage
 # run with "fab -H <host> <func>"
 
-from fabric.api import sudo, run
+from fabric.api import sudo, run, cd
 
 
 def intall_git():
-    sudo("apt-get update & apt-get install git-core -y")
-    
+    sudo("apt-get update && apt-get install git-core -y")
+
 
 def install_oh_my_zsh():
-    sudo("apt-get update & apt-get install zsh git-core -y")
-    run("wget --no-check-certificate http://install.ohmyz.sh -O - | sh")
-    run("chsh -s `which zsh`")
-    sudo("shutdown -l") #logout
+    sudo("apt-get update && apt-get install zsh git-core -y")
+    run("wget https://raw.github.com/robbyrussell/oh-my-zsh/"
+        "master/tools/install.sh -O install-oh-my-zsh.sh")
+    run("sed -i \"/chsh -s $(grep /zsh$ /etc/shells | tail -1)/"
+        " s/$/ `cut -d: -f1 /etc/passwd | tail -1`\" install-oh-my-zsh.sh")
+    run("chsh -s `which zsh` `whoami`")
+    run(". ./install_oh_my_zsh.sh")
+    run("rm ./install_oh_my_zsh.sh")
+    # sudo("shutdown -l")  # logout
+
+
+def download_zshrc():
+    url = "https://github.com/ibotdotout/dotfiles/blob/" \
+        "master/oh-my-zsh/.zshrc"
+    url_local = "https://github.com/ibotdotout/dotfiles/blob/" \
+        "master/oh-my-zsh/.zshrc.local"
+    run("wget %s" % url)
+    run("wget %s" % url_local)
 
 
 def install_vim():
-    sudo("apt-get update & apt-get install vim -y")
+    sudo("apt-get update && apt-get install vim -y")
 
 
 def download_vimrc():
     url = "https://raw.githubusercontent.com/" \
-          "ibotdotout/.devenv/master/vim/.vimrc"
+        "ibotdotout/.devenv/master/vim/.vimrc"
     run("wget %s" % url)
 
 
 def install_janus_vim():
     sudo("apt-get install ruby-dev rake exuberant-ctags ack-grep git-core -y")
     run("wget https://bit.ly/janus-bootstrap -O - | bash")
-    
+
+
 def download_janus_vim_conf():
-    sudo("pip install flake8 pylint") #To support python syntastic
+    sudo("pip install flake8 pylint")  # To support python syntastic
     url = "https://raw.githubusercontent.com/" \
-          "ibotdotout/.devenv/master/janus/.vimrc.after"
+        "ibotdotout/.devenv/master/janus/.vimrc.after"
     run("wget %s" % url)
     url = "https://raw.githubusercontent.com/" \
-          "ibotdotout/.devenv/master/janus/.vimrc.before"
+        "ibotdotout/.devenv/master/janus/.vimrc.before"
     run("wget %s" % url)
     run("mkdir -p ~/.janus")
     with cd('~/.janus'):
         run("git clone https://github.com/altercation/vim-colors-solarized.git")
         run("git clone https://github.com/Lokaltog/vim-powerline.git")
-        
+
 
 def install_tmux():
-    sudo("apt-get update & apt-get install tmux -y")
+    sudo("apt-get update && apt-get install tmux -y")
 
 
 def download_tmux_conf():
-    sudo("apt-get update & apt-get install xclip -y")
+    sudo("apt-get update && apt-get install xclip -y")
     url = "https://raw.githubusercontent.com/" \
-          "ibotdotout/.devenv/master/tmux/.tmux.conf"
+        "ibotdotout/.devenv/master/tmux/.tmux.conf"
     run("wget %s" % url)
 
 
 def install_python_pip():
-    sudo("apt-get update & apt-get install python-pip -y")
+    sudo("apt-get update && apt-get install python-pip -y")
 
 
 def install_python_virtualenv():
@@ -71,19 +86,38 @@ def install_python_virtualenv():
     run('echo "WORKON_HOME=~/Envs" >> %s' % bashrc)
     run('mkdir ~/Envs -p')
     run('echo "source /usr/local/bin/virtualenvwrapper.sh" >> %s' % bashrc)
-    
-    
+
+
 def install_docker_io():
-    sudo("apt-get update & apt-get install docker.io -y")
+    sudo("apt-get update && apt-get install docker.io -y")
     sudo("gpasswd -a ${USER} docker")
     sudo("service docker restart")
 
 
+def install_wemux():
+    sudo("git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux")
+    sudo("ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux")
+    sudo("cp /usr/local/share/wemux/wemux.conf.example "
+         "/usr/local/etc/wemux.conf")
+    sudo("sh -c 'echo \"host_list=(`cut -d: -f1 /etc/passwd | tail -1`)\""
+         " >> /usr/local/etc/wemux.conf'")
+
+
 def set_devenv():
+    intall_git()
+    install_tmux()
     install_vim()
     download_vimrc()
-    install_tmux()
     download_tmux_conf()
     install_python_pip()
     install_python_virtualenv()
-    intall_git()
+
+
+def set_koding():
+    # set enviorment for koding.io
+    install_oh_my_zsh()
+    install_wemux()
+    install_python_pip()
+    install_janus_vim()
+    download_janus_vim_conf()
+    download_tmux_conf()
